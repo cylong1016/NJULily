a03aee057b2623008d8745e4dbb754368d887ac0
cylong1016 <cyl19941016@sina.com>
2014-10-31 15:50:03 +0800

    添加商品条目的PO和VO

---
+++ src/main/java/po/CommodityItermPO.java
@@ -0,0 +1,79 @@
+package po;
+
+/**
+ * 商品条目
+ * @author cylong
+ * @version Oct 31, 2014 3:26:07 PM
+ */
+public class CommodityItermPO extends PersistentObject {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 1L;
+	/** 商品名称 */
+	private String name;
+	/** 商品型号 */
+	private String type;
+	/** 商品数量 */
+	private int number;
+	/** 商品 单价 */
+	private double price;
+	/** 总价 */
+	private double total;
+	/** 商品备注 */
+	private String comment;
+
+	public CommodityItermPO(String id, String name, String type, int number, double price, String comment) {
+		super(id);
+		this.name = name;
+		this.type = type;
+		this.number = number;
+		this.price = price;
+		this.comment = comment;
+		this.total = price * number;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getComment() {
+		return this.comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public double getTotal() {
+		return this.total;
+	}
+
+}
---
+++ src/main/java/vo/CommodityItermVO.java
@@ -0,0 +1,33 @@
+package vo;
+
+/**
+ * 商品条目
+ * @author cylong
+ * @version Oct 31, 2014 3:41:16 PM
+ */
+public class CommodityItermVO extends ValueObject {
+
+	/** 商品名称 */
+	public String name;
+	/** 商品型号 */
+	public String type;
+	/** 商品数量 */
+	public int number;
+	/** 商品 单价 */
+	public double price;
+	/** 总价 */
+	public double total;
+	/** 商品备注 */
+	public String comment;
+
+	public CommodityItermVO(String id, String name, String type, int number, double price, double total, String comment) {
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.number = number;
+		this.price = price;
+		this.total = total;
+		this.comment = comment;
+	}
+
+}
