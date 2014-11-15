bf76246b12b73f5afc5949724b6ff8cbb1c87d5f
cylong1016 <cyl19941016@sina.com>
2014-11-15 15:08:04 +0800

    更新详细设计描述文档和ApprovalDataService
    文档添加AccountBillbl模块；ApprovalDataService添加insert、delete、和getAllBills方法【添加需要审批的单子、删除需要审批的单子、得到全部需要审批的单子】

--- src/main/java/dataservice/ApprovalDataService.java
+++ src/main/java/dataservice/ApprovalDataService.java
@@ -1,5 +1,7 @@
 package dataservice;
 
+import java.util.ArrayList;
+
 import message.ResultMessage;
 import po.PersistentObject;
 
@@ -16,5 +18,30 @@ public interface ApprovalDataService extends DataService {
 	 * @return 处理结果
 	 */
 	public ResultMessage update(PersistentObject po);
+
+	/**
+	 * 添加一个需要审批的单据
+	 * @param po 需要审批的单据
+	 * @return 处理信息
+	 * @author cylong
+	 * @version Nov 15, 2014 2:54:57 PM
+	 */
+	public ResultMessage insert(PersistentObject po);
+
+	/**
+	 * 审批过后删除这个单子
+	 * @param po
+	 * @return 处理信息
+	 * @author cylong
+	 * @version Nov 15, 2014 3:02:38 PM
+	 */
+	public ResultMessage delete(PersistentObject po);
+
+	/**
+	 * @return 全部需要审批的单据
+	 * @author cylong
+	 * @version Nov 15, 2014 2:55:31 PM
+	 */
+	public ArrayList<PersistentObject> getAllBills();
 
 }
