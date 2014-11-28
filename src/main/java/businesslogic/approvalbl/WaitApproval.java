166e5a6fee87ba76475cc296b328a169051c4261
cylong1016 <cyl19941016@sina.com>
2014-11-28 12:43:23 +0800

    添加config�
    所有配置写在这里，包括界面和逻辑层的配置

--- src/main/java/businesslogic/approvalbl/WaitApproval.java
+++ src/main/java/businesslogic/approvalbl/WaitApproval.java
@@ -2,20 +2,21 @@ package businesslogic.approvalbl;
 
 import java.rmi.Naming;
 
+import config.RMI;
 import dataservice.ApprovalDataService;
 import dataservice.DataFactoryService;
 
 public class WaitApproval {
-	
-	public ApprovalDataService getApprovalData(){
+
+	public ApprovalDataService getApprovalData() {
 		try {
-			DataFactoryService factory = (DataFactoryService)Naming.lookup("rmi://127.0.0.1:8888/factory");
+			DataFactoryService factory = (DataFactoryService)Naming.lookup(RMI.URL);
 			ApprovalDataService approvalData = (ApprovalDataService)factory.getApprovalData();
 			return approvalData;
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		} 
+		}
 	}
 }
---
+++ src/main/java/config/RMI.java
@@ -0,0 +1,11 @@
+package config;
+
+
+/**
+ * 
+ * @author cylong
+ * @version 2014年11月28日  下午12:38:53
+ */
+public class RMI {
+	public static final String URL = "rmi://127.0.0.1:8888/factory";
+}
