86dde9373d594c1409176d934e6b9826545ee31d
cylong1016 <cyl19941016@sina.com>
2014-12-01 17:41:13 +0800

    添加了一个user，更改了Info，没有错误了
    UserID：2014
    Username：njulily
    Userpass：njulily

--- src/main/java/businesslogic/common/Info.java
+++ src/main/java/businesslogic/common/Info.java
@@ -4,30 +4,29 @@ import java.util.ArrayList;
 
 import dataenum.BillType;
 import dataenum.Storage;
-import dataservice.commondata.DataService;
+import dataservice.commondata.DataInfo;
 
-public abstract class Info<Data extends DataService<PersistentObject>> {
-	
+public abstract class Info<Data extends DataInfo> {
+
 	protected abstract Data getData();
-	
+
 	protected ArrayList<String> getID(String ID, String clientName, String salesman, Storage storage, BillType type) {
 		ArrayList<String> IDs = new ArrayList<String>();
 		Data data = getData();
 		if (ID != null) {
-			for (String id : data.getAllID(type)) {
+			for(String id : data.getAllID(type)) {
 				if (id.contains(ID)) {
 					IDs.add(id);
 				}
 			}
-		}
-		else {
+		} else {
 			IDs = data.getAllID(type);
 		}
 		if (clientName != null) {
 			if (IDs.isEmpty()) {
 				return IDs;
 			}
-			for (String id : IDs) {
+			for(String id : IDs) {
 				if (data.getClient(id) == null) {
 					continue;
 				}
@@ -40,8 +39,8 @@ public abstract class Info<Data extends DataService<PersistentObject>> {
 			if (IDs.isEmpty()) {
 				return IDs;
 			}
-			for (String id : IDs) {
-				if (data.getSalesman == null) {
+			for(String id : IDs) {
+				if (data.getSalesman(id) == null) {
 					continue;
 				}
 				if (!data.getSalesman(id).equals(salesman)) {
@@ -49,12 +48,12 @@ public abstract class Info<Data extends DataService<PersistentObject>> {
 				}
 			}
 		}
-		
+
 		if (storage != null) {
 			if (IDs.isEmpty()) {
 				return IDs;
 			}
-			for (String id : IDs) {
+			for(String id : IDs) {
 				if (data.getStorage(id) == null) {
 					continue;
 				}
@@ -65,5 +64,5 @@ public abstract class Info<Data extends DataService<PersistentObject>> {
 		}
 		return IDs;
 	}
-	
+
 }
--- src/main/java/dataservice/commondata/DataInfo.java
+++ src/main/java/dataservice/commondata/DataInfo.java
@@ -2,20 +2,49 @@ package dataservice.commondata;
 
 import java.util.ArrayList;
 
+import dataenum.BillType;
 import dataenum.Storage;
 
 /**
- * 
+ * 查看销售明细表，经营情况表时候返回信息
  * @author cylong
  * @version 2014年12月1日  下午4:03:20
  */
 public interface DataInfo {
 	
-	public ArrayList<String> getAllID();
+	/**
+	 * 根据单据类型返回全部的单据ID
+	 * @param type
+	 * @return ID的ArrayList
+	 * @author cylong
+	 * @version 2014年12月1日  下午5:26:46
+	 */
+	public ArrayList<String> getAllID(BillType type);
 	
+	/**
+	 * 根据单据ID查找其中的用户
+	 * @param billID
+	 * @return 用户名（name）
+	 * @author cylong
+	 * @version 2014年12月1日  下午5:24:26
+	 */
 	public String getClient(String billID);
 	
+	/**
+	 * 根据单据ID查找其中的销售人员
+	 * @param billID
+	 * @return 销售人员的名字（name）
+	 * @author cylong
+	 * @version 2014年12月1日  下午5:25:39
+	 */
 	public String getSalesman(String billID);
 	
+	/**
+	 * 根据单据ID查找其中的仓库
+	 * @param billID
+	 * @return Storage
+	 * @author cylong
+	 * @version 2014年12月1日  下午5:26:18
+	 */
 	public Storage getStorage(String billID);
 }
