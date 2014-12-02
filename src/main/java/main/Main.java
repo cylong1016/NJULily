0314c430243908ab3cc73070a40ac42b01ec8e3d
david941211 <657834200@qq.com>
2014-12-02 22:51:44 +0800

    我又改了什么

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 
-import ui.differui.inventory.frame.Frame_Inventory;
+import ui.commonui.login.Frame_Login;
 
 /**
  * @author cylong
@@ -11,14 +11,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//UI test
-//		Frame_Login lf = new Frame_Login();
-//		lf.setVisible(true);
-		
-		Frame_Inventory fm = new Frame_Inventory();
-		fm.setVisible(true);
-		
-//		Frame_Salesman fs = new Frame_Salesman();
-//		fs.setVisible(true);
-		
+		Frame_Login lf = new Frame_Login();
+		lf.setVisible(true);
+	
 	}
 }
