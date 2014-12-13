e2c4bdec415e2f14bd036183208d76249fc157da
cylong1016 <cyl19941016@sina.com>
2014-12-14 05:31:17 +0800

    ResultMessage中添加了远程调用异常

--- src/main/java/message/ResultMessage.java
+++ src/main/java/message/ResultMessage.java
@@ -7,8 +7,14 @@ package message;
  * @version Oct 26, 2014  4:29:45 PM
  */
 public enum ResultMessage {
-	SUCCESS,
-	FAILURE,
-	COMMODITY_LACK,
+	SUCCESS("成功"),
+	FAILURE("失败"),
+	REMOTE_EXCEPTION("远程调用异常"),
+	COMMODITY_LACK(""); // TODO
 	
+	public final String value;
+
+	ResultMessage(String value) {
+		this.value = value;
+	}
 }
