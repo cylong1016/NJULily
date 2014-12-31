a2528fa600ee86aa5d7d208b5e6ae543e9763531
david941211 <657834200@qq.com>
2014-12-31 19:23:12 +0800

    update

--- src/test/java/Record/testPro.java
+++ src/test/java/Record/testPro.java
@@ -1,21 +1,28 @@
 package Record;
 
 import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 import org.junit.Test;
 
+import config.RMIConfig;
+import vo.AccountBillVO;
 import vo.ValueObject;
+import businesslogic.accountbillbl.AccountBillShow;
 import businesslogic.recordbl.BusinessProList;
 import dataenum.BillType;
+import dataservice.accountbilldataservice.AccountBillDataService;
+import dataservice.accountbilldataservice.AccountBillInfoService;
+import dataservice.purchasedataservice.PurchaseInfoService;
 
 public class testPro {
 
 	@Test
 	public void test() throws RemoteException, MalformedURLException, NotBoundException {
-//		AccountBillDataService accountBillData = (AccountBillDataService)Naming.lookup(RMIConfig.PREFIX + AccountBillDataService.NAME);
+		AccountBillDataService accountBillData = (AccountBillDataService)Naming.lookup(RMIConfig.PREFIX + AccountBillDataService.NAME);
 //		ArrayList<AccountBillPO> pos = accountBillData.show();
 //		for (AccountBillPO po : pos) {
 //			System.out.println(po.getID());
@@ -23,15 +30,27 @@ public class testPro {
 //		AccountBillInfoService accountBill = (AccountBillInfoService)Naming.lookup(RMIConfig.PREFIX+AccountBillInfoService.NAME);
 //		ArrayList<String> IDs = accountBill.getAllID(BillType.EXPENSE);
 //		PurchaseInfoService purInfo = (PurchaseInfoService)Naming.lookup(RMIConfig.PREFIX+PurchaseInfoService.NAME);
-//		ArrayList<String> IDs = purInfo.getAllID(BillType.PURCHASE);
-//		for (String id : IDs) {
-//			System.out.println(id);
-//		}
-		BusinessProList pro = new BusinessProList(null, null);
-		pro.setInfo(BillType.EXPENSE, null, null, null);
-		ArrayList<ValueObject> vos = pro.getBusinessPro();
-		for (ValueObject vo : vos) {
-			System.out.println(vo.ID);
+		AccountBillShow show = new AccountBillShow();
+		ArrayList<AccountBillVO> pos = show.showExpense();
+		ArrayList<String> IDs = new ArrayList<String>();
+		for (AccountBillVO id : pos) {
+			System.out.println(id.ID);
+			IDs.add(id.ID);
 		}
+		ArrayList<AccountBillVO> vos = show.showPay();
+		for (AccountBillVO id : vos) {
+			System.out.println(id.ID);
+			IDs.add(id.ID);
+		}	
+		for (String id : IDs) {
+			System.out.println(id);
+			accountBillData.find(id);
+		}
+//		BusinessProList pro = new BusinessProList(null, null);
+//		pro.setInfo(BillType.PAY, null, null, null);
+//		ArrayList<ValueObject> vos = pro.getBusinessPro();
+//		for (ValueObject vo : vos) {
+//			System.out.println(vo.ID);
+//		}
 	}
 }
